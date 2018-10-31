59d39bdec1164eef654c1f02f999cb4bd3fd4eff
AJ Segura <segura.antonioj@gmail.com>
2018-10-31 01:40:57 +0100

    old folder deleted

--- utc/tests/testthat/test_utc_difference.R
+++
@@ -1,8 +0,0 @@
-library(utc)
-
-test_that("Difference_between_UTC_and_local_is_correct",
-           expect_equal(
-             format( - hoursUTC() %h+% Sys.time(), format = "%Y-%m-%d %H:%M:%S" ),
-             format( toUTC(Sys.time()), format = "%Y-%m-%d %H:%M:%S" )
-           )
-)
